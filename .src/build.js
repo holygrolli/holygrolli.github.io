f105b53826ddff7ebc2334427a22fd4a70aae7d5
Andreas Groll <andreas.groll@gmail.com>
2017-01-21 18:53:09 +0100

    added NODE_ENV switch between dev and prod

--- .src/build.js
+++ .src/build.js
@@ -6,6 +6,8 @@ var handlebars = require('handlebars');
 var collections = require('metalsmith-collections');
 var permalinks = require('metalsmith-permalinks');
 var moment = require('moment');
+var env = require('metalsmith-env');
+
 moment.locale('de');
 
 var serve = require('metalsmith-serve');
@@ -14,14 +16,41 @@ var watch = require('metalsmith-watch');
 handlebars.registerHelper('moment', function(time,format){
   return moment(time).format(format);
 });
+handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+  switch (operator) {
+    case '==':
+    return (v1 == v2) ? options.fn(this) : options.inverse(this);
+    case '===':
+    return (v1 === v2) ? options.fn(this) : options.inverse(this);
+    case '!=':
+    return (v1 != v2) ? options.fn(this) : options.inverse(this);
+    case '!==':
+    return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+    case '<':
+    return (v1 < v2) ? options.fn(this) : options.inverse(this);
+    case '<=':
+    return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+    case '>':
+    return (v1 > v2) ? options.fn(this) : options.inverse(this);
+    case '>=':
+    return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+    case '&&':
+    return (v1 && v2) ? options.fn(this) : options.inverse(this);
+    case '||':
+    return (v1 || v2) ? options.fn(this) : options.inverse(this);
+    default:
+    return options.inverse(this);
+  }
+});
 
-metalsmith(__dirname)
+var m = metalsmith(__dirname)
 .metadata({
   site: {
     name: 'ChaosBlog',
     description: "ChaosBlog ist der Blog von Andreas Groll und das (chaotische) Leben"
   }
 })
+.use(env())
 .use(collections({
   posts: {
     pattern: 'posts/**/*.md',
@@ -55,20 +84,24 @@ metalsmith(__dirname)
 }))
 .source('./files')
 .destination('../.')
-.clean(false)
-.use(serve({
-  port: 8080,
-  host: "0.0.0.0",
-  verbose: true
-}))
-.use(watch({
-  paths: {
-    "./files/**/*": true,
-    "./layouts/**/*": "**/*",
-  },
+.clean(false);
+
+if (process.env.NODE_ENV === 'development')
+{
+  m.use(serve({
+    port: 8080,
+    host: "0.0.0.0",
+    verbose: true
+  }))
+  .use(watch({
+    paths: {
+      "./files/**/*": true,
+      "./layouts/**/*": "**/*",
+    },
   // log: function(...args) { console.log("watchlog", args)}
-}))
-.build(function (err) {
+}));
+}
+m.build(function (err) {
   if (err) {
     console.log(err);
   }
